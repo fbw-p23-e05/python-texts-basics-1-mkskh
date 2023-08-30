999901c0076e699af0264ddd24dccaa662d15a7d
mkskh <maksym.kharchenko@dci-student.org>
2023-08-30 14:36:58 +0200

    done

---
+++ task3.py
@@ -0,0 +1,7 @@
+city = 'London'
+population = '9000000'
+
+check = True if type(city) == str else False
+
+print(f'City: {city} ({check})')
+print(f'Population: {population}')
---
+++ task4.py
@@ -0,0 +1,4 @@
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+
+result = text.index('capital')
+print('capital:', result)
